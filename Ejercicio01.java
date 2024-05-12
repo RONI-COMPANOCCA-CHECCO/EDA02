6cf85ca20c339032361b5d1d8ccfa561d453fcdc
rcompanocca <ronicompanocca@gmail.com>
2024-05-12 17:11:42 -0500

    Ejercicio 01

--- Ejercicio01.java
+++ Ejercicio01.java
@@ -1,10 +1,46 @@
-public int[] invertirArray(int[] A) {
-    int n = A.length;
-    int[] Ain = new int[n];
+import java.util.Scanner;
 
-    for (int i = 0; i < n; i++) {
-        Ain[i] = A[n - 1 - i];
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Solicitar al usuario que ingrese la cantidad de números
+        System.out.print("Ingrese cuántos números desea agregar: ");
+        int n = scanner.nextInt();
+        int[] A = new int[n]; // Creamos una matriz de tamaño n
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingrese un numero " + (i + 1) + ": ");
+            A[i] = scanner.nextInt();
+        }
+        
+        int[] Ain = Invertir(A);
+        
+        System.out.print("Matriz original: "); //  LA MATRIZ ORIGINAL
+        imprimirArray(A);
+        
+        System.out.print("Matriz invertida: ");
+        imprimirArray(Ain);
+        
+        scanner.close();
     }
+    
+    public static int[] Invertir(int[] A) {
+        int n = A.length;
+        int[] Ain = new int[n];
 
-    return Ain;
+        for (int i = 0; i < n; i++) {
+            Ain[i] = A[n - 1 - i];
+        }
+
+        return Ain;
+    }
+    
+    public static void imprimirArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
+
